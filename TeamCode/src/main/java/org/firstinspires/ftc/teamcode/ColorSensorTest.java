f98bfbd8f1156cfd817b1b5ce6b9e8358d0e7cd9
GroUfi <natthaphonpitukwongroj@gmail.com>
2026-01-03 15:09:43 +0700

    Create ColorSensorTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.machanisms.TestBenchColor;
+
+public class ColorSensorTest extends OpMode {
+    TestBenchColor bench = new TestBenchColor();
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        bench.getDetectedColor(telemetry);
+    }
+}
